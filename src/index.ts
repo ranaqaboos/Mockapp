507cca8b20bacd7e890fe2ccb7d0690dace50b03
ranaqaboos <106505615+ranaqaboos@users.noreply.github.com>
2022-06-26 23:18:18 +0300

    homework 2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { server } from './server';
 
-const port: any = process.env.$PORT ?? 3000
+const port: any = process.env.PORT ?? process.env.$PORT ?? 3002;
+const host = process.env.HOST || "0.0.0.0";
 
 server
 	.listen({
-		port: port,
-		host: '0.0.0.0',
+		port,
+		host
 	})
 	.catch((err) => {
 		server.log.error(err);
